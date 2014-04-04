7246b596d24348623e9a24fd0eacaa5ea90b8b5c
rj <none@none>
2014-04-03 21:35:34 -0400

    Reworked the interface to the cookie policy.  Users can no longer login or logout users without providing the http.Response.

--- cookie.go
+++ cookie.go
@@ -58,6 +58,10 @@ func (pq cookiePriorityQueue) MinValue() int64 {
 // on the client to verify authorized clients.  This authentication scheme
 // is more involved than the others, as callers will need to implement URLs
 // for login and logout pages.
+//
+// When a user successfully logs in, a token (nonce) is saved in a cookie.
+// The presence and validity of that token is verified to authorize future
+// HTTP requests.  The tokens can also be invalidated to logout a users.
 type Cookie struct {
 	// Realm provides a 'namespace' where the authentication will be considered.
 	Realm string
@@ -157,12 +161,16 @@ func (a *Cookie) NotifyAuthRequired(w http.ResponseWriter, r *http.Request) {
 	a.evictLeastRecentlySeen()
 }
 
-// Login checks the credentials of a client, and, if valid, creates a client
-// entry.  The nonce can be used by the client to identify the session.
-// Most callers will most likely be interested in LoginWithResponse.
+// The function createSession checks the credentials of a client, and, if
+// valid, creates a client entry.  The nonce can be used by the client to 
+// identify the session.
+//
+// This functions handles internal details of creating the session only.
+// The caller is still responsible for creating the HTTP response, which
+// will need to save the returned nonce.
 //
 // If the credentials cannot be verified, an error will be returned (ErrBadUsernameOrPassword).
-func (a *Cookie) Login(username, password string) (nonce string, err error) {
+func (a *Cookie) createSession(username, password string) (nonce string, err error) {
 	// Authorize the user
 	if !a.Auth(username, password) {
 		return "", ErrBadUsernameOrPassword
@@ -191,28 +199,30 @@ func (a *Cookie) Login(username, password string) (nonce string, err error) {
 	return nonce, nil
 }
 
-// LoginWithResponse checks the credentials of the client.  If successful, a
-// cookie is on the response so that the client can access the session again.
+// Login checks the credentials (a username/password pair) of the client.  
+// If successful, a session is created, and then a cookie is set on the 
+// HTTP response so that the client can access the session in future
+// HTTP requests.
 //
-// The caller is responsable for create an appropriate response to the HTTP request.
-// For successful validation, redirection (http.StatusTemporaryRedirect) to the
-// protected content is most likely the correct response.
+// The caller is responsable for creating an appropriate response body for
+// the HTTP request. For successful validation, redirection (http.StatusTemporaryRedirect) 
+// to the protected content is most likely the correct response.
 //
-// If the credentials cannot be verified, an error (ErrBadUsernameOrPassword) is
-// returned.  The caller is then responsable for creating an appropriate reponse to
-// the HTTP request.
-func (a *Cookie) LoginWithResponse(w http.ResponseWriter, username, password string) error {
-	nonce, err := a.Login(username, password)
+// If the credentials cannot be verified, an error (ErrBadUsernameOrPassword)
+// is returned.  Other errors are possible.  The caller is then responsable
+// for creating an appropriate reponse to the HTTP request.
+func (a *Cookie) Login(w http.ResponseWriter, username, password string) error {
+	nonce, err := a.createSession(username, password)
 	if err != nil {
 		return err
 	}
 
-	http.SetCookie(w, &http.Cookie{Name: "Authorization", Value: nonce, Path: a.Path})
+	http.SetCookie(w, &http.Cookie{Name: "Authorization", Value: nonce, Path: a.Path, HttpOnly:true})
 	return nil
 }
 
-// Logout ensures that the nonce is no longer valid.
-func (a *Cookie) Logout(nonce string) {
+// The function destroySession ensures that the nonce is no longer valid.
+func (a *Cookie) destroySession(nonce string) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 
@@ -226,15 +236,25 @@ func (a *Cookie) Logout(nonce string) {
 	}
 }
 
-// LogoutWithResponse ensures that the session associated with the HTTP request
-// is no longer valid.  It sets a header on the response to erase any cookies
-// used by the client to identify the session.
-func (a *Cookie) LogoutWithReponse(w http.ResponseWriter, r *http.Request) error {
+// Logout ensures that the session associated with the HTTP request
+// is no longer valid.  It then sets a header on the response to erase any 
+// cookies used by the client to identify the session.  However, even if
+// future HTTP requests contains the cookie, the call to Authorize will
+// fail.
+//
+// The caller is responsable for create an appropriate response body for the HTTP request.
+// When the function is successful, redirection (http.StatusTemporaryRedirect) to the
+// a login page or public content is most likely the correct response.
+//
+// If the credentials cannot be verified, an error (ErrBadUsernameOrPassword) is
+// returned.  The caller is then responsable for creating an appropriate reponse to
+// the HTTP request.
+func (a *Cookie) Logout(w http.ResponseWriter, r *http.Request) error {
 	// Find the nonce used to identify a client
 	token, err := r.Cookie("Authorization")
 	if err == nil || token.Value != "" {
 		// Invalidate the nonce
-		a.Logout(token.Value)
+		a.destroySession(token.Value)
 	}
 
 	// Clear the cookie from the client
